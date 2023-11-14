a2ec3ccadd29dc8485d99d0c107e53820f7bdb7a
Glenn Hermans <glhermans@gmail.com>
2023-11-14 12:35:02 +0100

    Refactor AzureConfigHelper

--- src/Helpers/AzureConfigHelper.php
+++ src/Helpers/AzureConfigHelper.php
@@ -4,13 +4,13 @@ namespace Bagisto\AzureAuth\Helpers;
 
 class AzureConfigHelper
 {
-    public static function isConfigured()
+    public static function isConfigured(): bool
     {
-        return (
-            config('services.azure.client_id') &&
-            config('services.azure.client_secret') &&
-            config('services.azure.redirect') &&
-            config('services.azure.tenant')
+        return !empty(
+            config('services.azure.client_id')
+            ?? config('services.azure.client_secret')
+            ?? config('services.azure.redirect')
+            ?? config('services.azure.tenant')
         );
     }
 }
